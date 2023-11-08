47314d98b66a8332bf89a064cad6c125e329b785
Maevis <128203386+Maevisxga@users.noreply.github.com>
2023-11-08 00:26:18 -0500

    Update SkyAuth.sql
    Updated the table to store the amount of time the player spent in the challenge until they died.

--- SQL/SkyAuth.sql
+++ SQL/SkyAuth.sql
@@ -1,11 +1,11 @@
-
 -- acore_auth
--- Listage de la structure de table acore_auth. hc_dead_log
+-- Estructura de la tabla acore_auth.hc_dead_log
 CREATE TABLE IF NOT EXISTS `hc_dead_log` (
   `username` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `level` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `killer` text COLLATE utf8mb4_general_ci,
   `date` datetime DEFAULT NULL,
   `result` text COLLATE utf8mb4_general_ci,
-  `guid` text COLLATE utf8mb4_general_ci
+  `guid` text COLLATE utf8mb4_general_ci,
+  `survival_time` text COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
