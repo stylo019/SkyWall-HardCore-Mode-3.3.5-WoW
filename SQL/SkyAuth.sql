a78b321c8859ba538459f300a5027d028943607e
Maevis <128203386+Maevisxga@users.noreply.github.com>
2023-11-08 15:14:14 -0500

    Update SkyAuth.sql

--- SQL/SkyAuth.sql
+++ SQL/SkyAuth.sql
@@ -1,5 +1,4 @@
 -- acore_auth
--- Estructura de la tabla acore_auth.hc_dead_log
 CREATE TABLE IF NOT EXISTS `hc_dead_log` (
   `username` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `level` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
@@ -7,5 +6,8 @@ CREATE TABLE IF NOT EXISTS `hc_dead_log` (
   `date` datetime DEFAULT NULL,
   `result` text COLLATE utf8mb4_general_ci,
   `guid` text COLLATE utf8mb4_general_ci,
-  `survival_time` text COLLATE utf8mb4_general_ci
+  `survival_time` text COLLATE utf8mb4_general_ci,
+  `player_race` text COLLATE utf8mb4_general_ci,
+  `player_class` text COLLATE utf8mb4_general_ci,
+  `zone_name` text COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
