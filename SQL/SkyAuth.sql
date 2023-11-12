4a13e39857e4c2eeb8d53751237d3c3cf8a0a5d8
Maevis <128203386+Maevisxga@users.noreply.github.com>
2023-11-12 11:31:53 -0500

    Update SkyAuth.sql

--- SQL/SkyAuth.sql
+++ SQL/SkyAuth.sql
@@ -1,13 +1,14 @@
 -- acore_auth
 CREATE TABLE IF NOT EXISTS `hc_dead_log` (
-  `username` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `level` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `killer` text COLLATE utf8mb4_general_ci,
-  `date` datetime DEFAULT NULL,
-  `result` text COLLATE utf8mb4_general_ci,
-  `guid` text COLLATE utf8mb4_general_ci,
-  `survival_time` text COLLATE utf8mb4_general_ci,
-  `player_race` text COLLATE utf8mb4_general_ci,
-  `player_class` text COLLATE utf8mb4_general_ci,
-  `zone_name` text COLLATE utf8mb4_general_ci
+  `username` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `level` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `killer` TEXT COLLATE utf8mb4_general_ci,
+  `date` DATETIME DEFAULT NULL,
+  `result` TEXT COLLATE utf8mb4_general_ci,
+  `guid` TEXT COLLATE utf8mb4_general_ci,
+  `survival_time` TEXT COLLATE utf8mb4_general_ci,
+  `player_race` TEXT COLLATE utf8mb4_general_ci,
+  `player_class` TEXT COLLATE utf8mb4_general_ci,
+  `zone_name` TEXT COLLATE utf8mb4_general_ci,
+  `death_quote` TEXT COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
