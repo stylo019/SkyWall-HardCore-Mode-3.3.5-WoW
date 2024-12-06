3a3313fac581ba890c01a7b4fb0e2482f62cfb1f
SkyWall Server <aydin_sari@live.fr>
2024-12-06 14:53:24 +0100

    New Version 3.3.5 for Azerothcore - SKYWALL.ORG

--- HardcoreMode.lua
+++ HardcoreMode.lua
@@ -1,8 +1,5 @@
 -- SkyWall.org
 
-local auraID = 43869  -- Replace with the actual ID of the aura you want to apply
-local auraDuration = 6  -- Replace with the desired duration of the aura in seconds
-
 local raceNames = {
     [1] = "Human",
     [2] = "Orc",
@@ -24,6 +21,7 @@ local classNames = {
     [7] = "Shaman",
     [8] = "Mage",
     [9] = "Warlock",
+    [11] = "Druid",
 }
 
 local deathQuotes = {
@@ -69,111 +67,106 @@ local deathQuotes = {
     "Final chord played, melody endures...",
 }
 
+local startQuotes = {
+    "Prepare for an epic journey!",
+    "May fortune favor you!",
+    "The adventure begins!",
+    "Brace yourself for the challenges ahead!",
+    "Your story starts now!",
+    "Embark on your epic quest!",
+    "The journey of a thousand miles begins!",
+    "Heroic deeds await you!",
+    "Good luck, brave soul!",
+    "May your path be filled with glory!",
+    "Adventure and danger lie ahead!",
+    "Step boldly into your destiny!",
+    "Your epic tale starts here!",
+    "Prepare for the trials ahead!",
+    "Your legend begins!",
+    "Embrace the challenge!",
+    "The road to greatness starts here!",
+    "Embark on your hardcore journey!",
+    "Greatness awaits you!",
+    "Forge your legacy!",
+}
+
 local function formatTime(seconds)
     local days = math.floor(seconds / 86400)
     local hours = math.floor((seconds % 86400) / 3600)
     local minutes = math.floor((seconds % 3600) / 60)
-    local seconds = seconds % 60
+    seconds = seconds % 60
     return string.format("%d days, %d hours, %02d min, %02d sec", days, hours, minutes, seconds)
 end
 
-local function CreateGrave(player)
-    if player:GetLevel() < 15 then
-        -- Tomb is not created for players below level 15
-        return
-    end
+local function BroadcastDeathMessage(player, killer)
+    local playerName = player:GetName():gsub("'", "''")
+    local playerLevel = player:GetLevel()
+    local playerRace = raceNames[player:GetRace()] or "Unknown"
+    local playerClass = classNames[player:GetClass()] or "Unknown"
+    local killerName = killer and killer:GetName() or "Unknown"
+    local formattedTimeLvl = formatTime(player:GetLevelPlayedTime())
+    local zoneName = player:GetMap():GetName()
+    local deathQuote = deathQuotes[math.random(1, #deathQuotes)]:gsub("'", "''")
 
-    local gameObjectEntry = 254605 -- Gameobject id of your grave // It is recommended to use this id 194537
-    local secondsInADay = 86400 -- Duración en segundos que la tumba estará en el mundo (1 día)
-
-    local x, y, z, o = player:GetLocation()
-
-    local grave = player:SummonGameObject(gameObjectEntry, x, y, z, o)
-
-    if grave then
-        local graveGUID = grave:GetGUIDLow() -- Obtener el GUID de la tumba
-
-        -- Depawn the grave
-        local event = CreateLuaEvent(function()
-            if grave and grave:IsInWorld() then
-                grave:Despawn()
-            end
-        end, secondsInADay * 1000, 1)
-
-        print("Grave GUID: " .. graveGUID)
-
-        -- Store the GUID of the grave in the database
-        local input_Grave_GUID = "UPDATE hc_dead_log SET grave_guid = '" .. graveGUID .. "' WHERE guid = '" .. player:GetGUIDLow() .. "'"
-        AuthDBExecute(input_Grave_GUID)
+    local message = string.format("|cFFffffffHardcore|r : |cFFffffffLevel %d player |cFF00ff00%s|r |cFFffffff(%s %s) was killed by |cFF00ff00%s|r |cFFffffffin the %s zone. %s|r", 
+                                  playerLevel, playerName, playerRace, playerClass, killerName, zoneName, formattedTimeLvl, deathQuote)
+    
+    for _, p in ipairs(GetPlayersInWorld()) do
+        p:SendBroadcastMessage(message)
+        p:SendAreaTriggerMessage(message)
     end
 end
 
+local function LogDeathToDatabase(player, killer)
+    local playerGUID = player:GetGUIDLow()
+    local playerName = player:GetName():gsub("'", "''")
+    local playerLevel = player:GetLevel()
+    local killerName = killer and killer:GetName() or "Unknown"
+    killerName = killerName:gsub("'", "''")
+
+    local query = string.format("INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('%s', '%d', '%s', NOW(), 'DEAD', '%d')", 
+                                playerName, playerLevel, killerName, playerGUID)
+    AuthDBExecute(query)
+end
+
+local function NotifyDiscord(player, killer)
+    local playerName = player:GetName()
+    local playerRace = raceNames[player:GetRace()] or "Unknown"
+    local playerClass = classNames[player:GetClass()] or "Unknown"
+    local playerLevel = player:GetLevel()
+    local zoneName = player:GetMap():GetName()
+    local formattedTimeLvl = formatTime(player:GetLevelPlayedTime())
+    local killerName = killer and killer:GetName() or "Unknown"
+    local deathQuote = deathQuotes[math.random(1, #deathQuotes)]
+
+    local embed = string.format('{"username": "Hardcore System", "avatar_url": "https://skywall.org/hclogo.png", "content": ":skull: Player **%s (%s %s)** was killed by **%s** at Level %d in the %s zone. %s :skull_crossbones:"}', 
+                                playerName, playerRace, playerClass, killerName, playerLevel, zoneName, deathQuote)
+    HttpRequest("POST", "https://discord.com/api/webhooks/1128075342033207336/dcg5e7hfOQ2dvU_rL-8oBSRFgDpyQeNM7HFPuDQXtXZ6yMDJ6tVJ1CUlUEqAJoYhT-Gi", embed, "application/json", function(status, body, headers)
+        print(body)
+    end)
+end
+
 local function PlayerDeath(event, killer, player)
-    if player:HasItem(666, 1) then
-        local playerGUID = player:GetGUIDLow()
-        local playerName = player:GetName()
-        local playerLevel = player:GetLevel()
-        local playerRaceID = player:GetRace()
-        local playerClassID = player:GetClass()
-        local currLevelPlayTime = player:GetLevelPlayedTime()
-        local formattedTimeLvl = formatTime(currLevelPlayTime)
-
-        local playerRace = raceNames[playerRaceID] or "Unknown"
-        local playerClass = classNames[playerClassID] or "Unknown"
-
-        local guild = player:GetGuild()
-        if guild and guild:GetName() == "HardCore" then
-            guild:DeleteMember(player, false)
-            SendWorldMessage("|cFFffffffHardcore|r : |cFF007bf6" .. playerName .. " was removed from the Hardcore Guild!|r")
-        end
-
-        local players = GetPlayersInWorld()
-        local killerName = killer and killer:GetName() or "Unknown"
-
-        local survivalTime = currLevelPlayTime  
-        local zoneName = player:GetMap():GetName()
-
-        local quoteIndex = math.random(1, #deathQuotes)
-        local deathQuote = deathQuotes[quoteIndex]
-
-        -- Escape single quotes in strings
-        playerName = playerName:gsub("'", "''")
-        killerName = killerName:gsub("'", "''")
-        deathQuote = deathQuote:gsub("'", "''")
-
-        player:AddAura(auraID, player, player):SetDuration(auraDuration * 1000)
-
-        -- Notify the player about the aura and the punishment
-        player:SendBroadcastMessage("|cFFFF0000Hardcore |r: |cFFC0C0C0You have failed the Hardcore challenge and are now under a penalty aura for " .. auraDuration .. " seconds. " .. "Reflect on your journey and try again!|r")
-
-        for _, p in ipairs(players) do
-            if p == player then
-                p:SendBroadcastMessage("|cFFffffffHardcore|r : |cFFffffffLevel " .. playerLevel .. " player |cFF00ff00" .. playerName .. "|r |cFFffffff(" .. playerRace .. " " .. playerClass .. ") was killed by |cFF00ff00" .. killerName .. "|r |cFFffffffin the " .. zoneName .. " zone, after surviving " .. formattedTimeLvl .. ". " .. deathQuote .. "|r")
-                p:SendAreaTriggerMessage("|cFFffffffHardcore|r : |cFFffffffLevel " .. playerLevel .. " player |cFF00ff00" .. playerName .. "|r |cFFffffff(" .. playerRace .. " " .. playerClass .. ") was killed by |cFF00ff00" .. killerName .. "|r |cFFffffffin the " .. zoneName .. " zone, after surviving " .. formattedTimeLvl .. ". " .. deathQuote .. "|r")
-            end
-        end
-
-        local input_HC_Dead = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid, survival_time, player_race, player_class, zone_name, death_quote) VALUES ('" .. playerName .. "', '" .. playerLevel .. "', '" .. killerName .. "',  NOW(), 'DEAD', '" .. playerGUID .. "', '" .. survivalTime .. "', '" .. playerRace .. "', '" .. playerClass .. "', '" .. zoneName .. "', '" .. deathQuote .. "')"
-        AuthDBExecute(input_HC_Dead)
-
-        player:RemoveItem(666, 1)
-
-        -- Create and spawn the grave after removing the item
-        CreateGrave(player)
-
-        -- Discord embed
-        local embed = '{"username": "Hardcore System", "avatar_url": "https://skywall.org/hclogo.png", "content": ":skull: Player **'.. playerName ..' (' .. playerRace .. ' ' .. playerClass .. ')** was killed by **' ..killerName.. '** at Level '.. playerLevel ..' in the ' .. zoneName .. ' zone, after surviving ' ..formattedTimeLvl.. '. ' .. deathQuote .. ' :skull_crossbones:"}'
-        -- POST request to Discord Webhook
-        HttpRequest("POST", "https://discord.com/api/webhooks/1175435360143159338/5eY7cwlOQ7xbr3MGswxRXM7zXeeBDHTyO2kxITsaFJqA0GFhWAJqYLgNLJrdktUvxhHp",
-            embed, "application/json", function(status, body, headers)
-                print(body)
-            end)
+    if type(player) ~= "userdata" or not player:HasItem(666, 1) then
+        return
     end
+
+    local guild = player:GetGuild()
+    if guild and guild:GetName() == "HardCore" then
+        guild:DeleteMember(player, false)
+        SendWorldMessage("|cFFffffffHardcore|r : |cFF007bf6" .. player:GetName() .. " was removed from the Hardcore Guild!|r")
+    end
+
+    BroadcastDeathMessage(player, killer)
+    LogDeathToDatabase(player, killer)
+    player:RemoveItem(666, 1)
+    --CreateGrave(player)
+    NotifyDiscord(player, killer)
 end
 
 RegisterPlayerEvent(6, PlayerDeath)
 
-local function OnFirstTalk(event, player, unit)
+local function HandleFirstTalk(event, player, unit)
     if player:GetLevel() == 1 then
         if player:HasItem(666) then
             player:GossipMenuAddItem(0, "Thanks!", 0, 3)
@@ -184,20 +177,6 @@ local function OnFirstTalk(event, player, unit)
         end
     else
         player:SendBroadcastMessage("|cFFffffffHardcore|r : Your current level is too high to participate in HC mode. In order to experience the thrill of HC, it is necessary to create a new hero.")
-            local function formatTime(seconds)
-                local days = math.floor(seconds / 86400)
-                local hours = math.floor((seconds % 86400) / 3600)
-                local minutes = math.floor((seconds % 3600) / 60)
-                local seconds = seconds % 60
-                return string.format("%01d days, %01d hours,%02d min.%02d sec.", days, hours, minutes, seconds)
-            end
-            local currLevelPlayTime = player:GetLevelPlayedTime()
-            local totalPlayTime = player:GetTotalPlayedTime()
-
-            local formattedTimeLvl = formatTime(currLevelPlayTime)
-            local formattedTimeTotal = formatTime(totalPlayTime)
-                SendWorldMessage("Total time played: " .. formattedTimeTotal)
-                SendWorldMessage("Total played this level: " .. formattedTimeLvl)
     end
 end
 
@@ -206,46 +185,41 @@ local function OnSelect(event, player, unit, sender, intid, code)
         player:GossipMenuAddItem(0, "Yes, these are my last words!", 0, 2)
         player:GossipMenuAddItem(0, "No, take me back!", 0, 3)
         player:GossipSendMenu(6667, unit)
-    end
-    if intid == 3 then
+    elseif intid == 3 then
         player:GossipComplete()
     end
+end
+
+local function StartHardCoreMode(player)
+    local playerName = player:GetName()
+    player:AddItem(666, 1)
+    player:SetCoinage(0)
+    player:SendAreaTriggerMessage("|cFFffffffWelcome to Hardcore Mode,|cFF00ff00" .. playerName .. ".|r |cFFffffffStay vigilant and tread carefully!|r")
+    SendWorldMessage("|cFFffffffHardcore|r : |cFF00ff00" .. playerName .. "|r has entered Hardcore Mode! Best of luck on your journey!")
+
+    local playerGUID = player:GetGUIDLow()
+    local startQuote = startQuotes[math.random(1, #startQuotes)]
+    local query = string.format("INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('%s', '%d', 'STARTED', NOW(), 'BEGIN', '%d')", 
+                                playerName, player:GetLevel(), playerGUID)
+    AuthDBExecute(query)
+
+    local embed = string.format('{"username": "Hardcore System", "avatar_url": "https://skywall.org/hclogo.png", "content": ":tada: Player **%s** has entered his HardCore Mode and receive his sign! %s :saluting_face:"}', 
+                                playerName, startQuote)
+    
+    HttpRequest("POST", "https://discord.com/api/webhooks/1128075342033207336/dcg5e7hfOQ2dvU_rL-8oBSRFgDpyQeNM7HFPuDQXtXZ6yMDJ6tVJ1CUlUEqAJoYhT-Gi", embed, "application/json", function(status, body, headers)
+        print(body)
+    end)
+
+    player:GossipComplete()
 end
 
 local function OnHardCore(event, player, unit, sender, intid, code)
     if intid == 2 then
-        local playerName = player:GetName()  -- Retrieve the player's name
-
-        player:AddItem(666, 1)
-        player:SetCoinage(0)
-        player:SendAreaTriggerMessage("|cFFffffffWelcome to Hardcore Mode,|cFF00ff00" .. playerName .. ".|r |cFFffffffStay vigilant and tread carefully!|r")
-        SendWorldMessage("|cFFffffffHardcore|r : |cFF00ff00" .. playerName .. "|r has entered Hardcore Mode! Best of luck on your journey!")
-
-        local playerGUID = player:GetGUIDLow()
-
-        -- Insert a record into the hc_dead_log table to mark the player's start
-        local input_HC_Start = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. playerName .. "', '" .. player:GetLevel() .. "', 'STARTED', NOW(), 'BEGIN', '" .. playerGUID .. "')"
-        AuthDBExecute(input_HC_Start)
-
-        -- Discord embed
-        local embed = '{"username": "Hardcore System", "avatar_url": "https://skywall.org/hclogo.png", "content": ":tada: Player **' .. playerName .. '** started his HardCore Mode! Good luck! :saluting_face:"}'
-        -- POST request to Discord Webhook
-        HttpRequest("POST", "https://discord.com/api/webhooks/1175435360143159338/5eY7cwlOQ7xbr3MGswxRXM7zXeeBDHTyO2kxITsaFJqA0GFhWAJqYLgNLJrdktUvxhHp",
-            embed, "application/json", function(status, body, headers)
-                print(body)
-            end)
-
-        -- Add the player to the "HardCore" guild using Guild:AddMember()
-        local guild = GetGuildByName("HardCore")
-        if guild then
-            guild:AddMember(player, 3) -- Replace 3 with the desired rank ID
-        end
-
-        player:GossipComplete()
+        StartHardCoreMode(player)
     end
 end
 
-RegisterCreatureGossipEvent(666, 1, OnFirstTalk)
+RegisterCreatureGossipEvent(666, 1, HandleFirstTalk)
 RegisterCreatureGossipEvent(666, 2, OnSelect)
 RegisterCreatureGossipEvent(666, 2, OnHardCore)
 RegisterPlayerEvent(8, PlayerDeath)
--- HardcoreRewards.lua
+++ HardcoreRewards.lua
@@ -1,130 +1,97 @@
---SKYWALL.ORG -- HC MODE -- / /
-local function onPlayerLevelUp(event, player, newLevel)
-  local function formatTime(seconds)
-    local minutes = math.floor(seconds / 60)
-    local hours = math.floor(minutes / 60)
-    local days = math.floor(hours / 24)
+-- SKYWALL.ORG -- HC MODE
 
-    local formattedTime = string.format("%dd, %02dh, %02dm", days, hours % 24, minutes % 60)
-    return formattedTime
+local function formatTime(seconds)
+  local minutes = math.floor(seconds / 60)
+  local hours = math.floor(minutes / 60)
+  local days = math.floor(hours / 24)
+
+  local formattedTime = string.format("%dd, %02dh, %02dm", days, hours % 24, minutes % 60)
+  return formattedTime
+end
+
+local function broadcastMessage(player, message)
+  local players = GetPlayersInWorld()
+  for _, p in ipairs(players) do
+      p:SendAreaTriggerMessage(message)
+  end
+end
+
+local function sendDiscordNotification(content)
+  local embed = '{"username": "Hardcore System", "avatar_url": "https://skywall.org/hclogo.png", "content": "' .. content .. '"}'
+  HttpRequest("POST", "https://discord.com/api/webhooks/1128075342033207336/dcg5e7hfOQ2dvU_rL-8oBSRFgDpyQeNM7HFPuDQXtXZ6yMDJ6tVJ1CUlUEqAJoYhT-Gi", embed, "application/json", function(status, body, headers)
+      print(body)
+  end)
+end
+
+local function logLevelUp(player, level)
+  local playerGUID = player:GetGUIDLow()
+  local insertDB = string.format("INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('%s', '%d', 'LEVELUP', NOW(), 'LEVELUP', '%d')", player:GetName(), level, playerGUID)
+  AuthDBExecute(insertDB)
+end
+
+local function handleLevelUp(player, newLevel, message, discordContent)
+  SendWorldMessage(message)
+  broadcastMessage(player, message)
+  sendDiscordNotification(discordContent)
+  logLevelUp(player, newLevel)
+end
+
+local function getLevelUpMessage(level, playerName)
+  local messages = {
+      [9] = "|cFFffffffHardcore|r : %s has reached level 10 without dying on Hardcore Mode! Congratulations!",
+      [14] = "|cFFffffffHardcore|r : %s has reached level 15 without dying on Hardcore Mode! Great job!",
+      [19] = "|cFFffffffHardcore|r : %s has reached level 20 without dying on Hardcore Mode! Keep it up!",
+      [29] = "|cFFffffffHardcore|r : %s has reached level 30 without dying on Hardcore Mode! Impressive!",
+      [49] = "|cFFffffffHardcore|r : %s has reached level 50 without dying on Hardcore Mode! Remarkable achievement!",
+      [59] = "|cFFffffffHardcore|r : %s has reached level 60 without dying on Hardcore Mode! You're a legend!",
+      [69] = "|cFFffffffHardcore|r : %s has reached level 70 without dying on Hardcore Mode! Incredible!",
+      [74] = "|cFFffffffHardcore|r : %s has reached level 75 without dying on Hardcore Mode! Unbelievable!",
+      [79] = "|cFFffffffHardcore|r : %s has reached the max level 80 without dying on Hardcore Mode! Congratulations, you are now Immortal!"
+  }
+  return string.format(messages[level], playerName)
+end
+
+local function getDiscordContent(level, playerName)
+  local contents = {
+      [9] = ":star: Player **%s** has reached level 10 without dying on Hardcore Mode! Congratulations!",
+      [14] = ":star: Player **%s** has reached level 15 without dying on Hardcore Mode! Great job!",
+      [19] = ":star: Player **%s** has reached level 20 without dying on Hardcore Mode! Keep it up!",
+      [29] = ":star: Player **%s** has reached level 30 without dying on Hardcore Mode! Impressive!",
+      [49] = ":star: Player **%s** has reached level 50 without dying on Hardcore Mode! Remarkable achievement!",
+      [59] = ":star: Player **%s** has reached level 60 without dying on Hardcore Mode! You're a legend!",
+      [69] = ":star: Player **%s** has reached level 70 without dying on Hardcore Mode! Incredible!",
+      [74] = ":star: Player **%s** has reached level 75 without dying on Hardcore Mode! Unbelievable!",
+      [79] = ":star: Player **%s** has reached the max level 80 without dying on Hardcore Mode! Congratulations, you are now Immortal!"
+  }
+  return string.format(contents[level], playerName)
+end
+
+local function onPlayerLevelUp(event, player, newLevel)
+  if not player:HasItem(666, 1) then
+      return
   end
 
-  if newLevel == 79 and player:HasItem(666, 1) then
-      player:AddItem(36941, 1) 
+  local playerName = player:GetName()
 
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached the max level 80 without dying on Hardcore Mode! Congratulations he is now Immortal!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 80 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
+  if newLevel == 79 then
+      player:AddItem(36941, 1)
+      handleLevelUp(player, newLevel, getLevelUpMessage(newLevel, playerName), getDiscordContent(newLevel, playerName))
+  elseif newLevel == 74 or newLevel == 69 or newLevel == 59 or newLevel == 49 or newLevel == 29 or newLevel == 19 or newLevel == 14 or newLevel == 9 then
+      handleLevelUp(player, newLevel, getLevelUpMessage(newLevel, playerName), getDiscordContent(newLevel, playerName))
+  elseif newLevel == 5 then
+      local guild = GetGuildByName("HardCore")
+      if guild then
+          guild:AddMember(player, 3)
+          broadcastMessage(player, string.format("|cFFffffff%s|r. Nice! You deserve to join the HardCore guild! |cFF00ff00Congratulations and Welcome!|r", playerName))
+          sendDiscordNotification(string.format(":loudspeaker: Player **%s** has joined the HardCore Guild!", playerName))
       end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
+  end
 
-    if newLevel == 74 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 75 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 75 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 69 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 70 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 70 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 59 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 60 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 60 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-    
-    if newLevel == 49 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 50 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 50 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 39 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 40 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 40 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 29 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 30 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 30 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 19 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 20 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 20 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel == 9 and player:HasItem(666, 1) then
-      SendWorldMessage("|cFFffffffHardcore|r : " .. player:GetName() .. " has reached level 10 without dying on Hardcore Mode! Congratulations!")
-      local players = GetPlayersInWorld()
-
-      for _, otherPlayer in ipairs(players) do
-              otherPlayer:SendAreaTriggerMessage("|cFFffffff" .. player:GetName() .. "|r has reached level 10 without dying on Hardcore Mode! |cFF00ff00Congratulations!|r")
-      end
-        local playerGUID = player:GetGUIDLow()
-        local isertDB = "INSERT INTO hc_dead_log (username, level, killer, date, result, guid) VALUES ('" .. player:GetName() .. "', '" .. player:GetLevel() .. "', 'LEVELUP', NOW(), 'LEVELUP', '" ..playerGUID.."')"
-        AuthDBExecute(isertDB)
-    end
-
-    if newLevel and player:HasItem(666, 1) then
+  if newLevel then
       local totalPlayTime = player:GetTotalPlayedTime()
       local formattedTimeTotal = formatTime(totalPlayTime)
-        SendWorldMessage("|cFFffffffHardcore|r : Total time played: " .. formattedTimeTotal)
-    end
-
+      SendWorldMessage("|cFFffffffHardcore|r : Total time played: " .. formattedTimeTotal)
+  end
 end
 
 RegisterPlayerEvent(13, onPlayerLevelUp)
